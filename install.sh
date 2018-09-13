45ef0628bcf06ec37165286ff07e9a0c2696c23e
Alex Kowalczuk <alex.kowalczuk@snapsheet.me>
2018-09-13 11:06:30 -0500

    Improvements to install script

--- install.sh
+++ install.sh
@@ -7,13 +7,23 @@ if ! type brew &> /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-echo "Installing tmux."
-brew install tmux
+echo "Checking for tmux..."
+if ! type tmux &> /dev/null; then
+  echo "Installing tmux."
+  brew install tmux
+else
+  echo "Already installed."
+fi
 
-echo "Installing bash-git-prompt via homebrew."
-brew install bash-git-prompt
+echo "Checking for bash-git-prompt..."
+if ! brew list bash-git-prompt &> /dev/null; then
+  echo "Installing bash-git-prompt via homebrew."
+  brew install bash-git-prompt
+else
+  echo "Already installed."
+fi
 
-echo "Install vim plugins."
+echo "Installing vim plugins."
 vim +PlugInstall +qall
 
 echo "Done."
